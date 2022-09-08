37e871b3d0ad6dbea82ee46654dced29a5a0872b
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-09-08 11:39:01 +0300

    LR1

---
+++ LR1-9/LR1-9/LR1_9.py
@@ -0,0 +1,8 @@
+#1
+a = int(input('Enter 1st groups: '))
+b = int(input('Enter 2st gropus: '))
+c = int(input('Enter 3st groups: '))
+
+print(f'Need table for 1st class {int(a / 2)}')
+print(f'Need table for 2st class {int(b / 2)}')
+print(f'Need table for 3st class {int(c / 2)}')
