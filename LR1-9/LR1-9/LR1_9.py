8ea9bdeaad09e532a31f96f6504e720cc42d32b8
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-09-08 12:51:55 +0300

    Update LR1_9.py

--- LR1-9/LR1-9/LR1_9.py
+++ LR1-9/LR1-9/LR1_9.py
@@ -6,3 +6,24 @@ c = int(input('Enter 3st groups: '))
 print(f'Need table for 1st class {int(a / 2)}')
 print(f'Need table for 2st class {int(b / 2)}')
 print(f'Need table for 3st class {int(c / 2)}')
+
+#2
+x = int(input('Enter 1st position: '))
+y = int(input('Enter 2st position: '))
+
+if x > 0 and x < 9 and y > 0 and y < 9:
+    print(f'Ladiya here {x},{y} ')
+
+    print('Enter pls')
+    x2 = int(input('Enter 1st move position: '))
+    y2 = int(input('Enter 3st move position: '))
+
+    if x2 > 0 and x2 < 9 and y2 > 0 and y2 < 9:
+        if x2 == x or y2 == y:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        print('Error')
+else:
+    print('Error')
