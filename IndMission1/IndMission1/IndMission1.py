fc7f7614c0c2a8d21eabcc2731ea25c55412582f
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-09-22 23:35:57 +0300

    Let me die (Не сделано)

--- IndMission1/IndMission1/IndMission1.py
+++ IndMission1/IndMission1/IndMission1.py
@@ -1,32 +1,60 @@
 f = open("10a.txt","r")
-n = int(f.read(3))
-k = int(f.read(3))
-d = int(f.read(3))
+f2 = open("10b.txt","r")
+#n = int(f.read(3))
+#k = int(f.read(3))
+#d = int(f.read(3))
+n = int(f2.read(6))
+k = int(f2.read(3))
+d = int(f2.read(4))
 f.read(1)
 print(n,k,d)
 test =[]
-tes1 =[]
-for numbers in f:
+test1 =[]
+for numbers in f2:
     test.append(int(numbers))
 
+count = -1
 for num in test:
+    count+=1
     if num < 0:
-        num *= -1
-        numb = ""
-        while num > 0:
-            numb += str(num % 3)
-            num//=3
-        print(numb[::-1])
-        sum = 0
-        mult = 1
-        while int(numb) > 0:
-            digit = int(numb) % 10
-            sum = sum + digit
-            mult = mult  * digit
-            int(numb) = int(numb) // 10
-        if sum == 12:
-            test1.append(int(sum,3) * -1)
+        num1 = num * -1
+        numb = 0
+        while num1 > 0:
+            numb += (num1 % 3)
+            num1//=3
+        if numb == 12:
+            test1.append(count)
+test1.append(count)
 
-print(test1)
+if (len(test1) % k != 0):
+    j = 0
+    suma = 0
+    count = 0
+    suma_max = []
+    for num5 in test:
+        count += 1
+        suma += num5
+        if test.index(num5) == test1[j]:
+            suma += num5
+            count -= 1
+            if (count % d == 0):
+                suma_max.append(suma)
+            count = 0
+            j += 1
+            suma = 0
+else:
+    j = 0
+    suma = 0
+    count = 0
+    suma_max = []
+    for num5 in test:
+        if test.index(num5) != test1[-1]:
+            count += 1
+            suma += num5
+        else:
+            break
+
+print(suma)
 
 f.close()
+f2.close()
