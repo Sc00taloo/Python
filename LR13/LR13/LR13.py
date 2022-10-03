9fa89aac310492ca9aa6e17aaf5391e8a0de0c06
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-10-03 12:20:25 +0300

    LR13.2

--- LR13/LR13/LR13.py
+++ LR13/LR13/LR13.py
@@ -1,3 +1,27 @@
+#2
+class Animal():
+    name=""
+    def __init__(self, newName):
+        self.name = newName
+        print("Роилось животное", newName)
+    def setName(self, newName):
+        self.name = newName
+    def getName(self):
+        return self.name
+    def eat(self):
+        print("Намнём")
+    def makeNoise(self):
+        print(self.name, "говорит Гррр")
+
+myAnimal = Animal("Baget")
+print(myAnimal.getName())
+
+myAnimal.setName("Packet")
+print(myAnimal.getName())
+
+myAnimal.eat()
+myAnimal.makeNoise()
+
 #1
 class Cat():
     name=""
