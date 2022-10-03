0604a8c1618a6ce08858a3525904a1dc6c72f845
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-10-03 12:47:59 +0300

    LR13.3

--- LR13/LR13/LR13.py
+++ LR13/LR13/LR13.py
@@ -1,3 +1,31 @@
+#3
+class StringVar():
+    def __init__(self, input_name):
+        self.hidden_name = input_name
+    #def get_name(self):
+    #    print('внутри getter')
+    #    return self.hidden_name
+    @property
+    def name(self):
+        print('внутри getter')
+        return self.hidden_name
+    #def set_name(self, input_name):
+    #    print('внутри setter')
+    #    self.hidden_name = input_name
+    @name.setter
+    def name(self, input_name):
+        print('внутри setter')
+        self.hidden_name = input_name
+    #name = property(get_name, set_name)
+
+test = StringVar("Eating")
+print(test.name)
+#print(test.get_name())
+test.name = "Siting"
+print(test.name)
+#test.set_name("Bruh")
+#print(test.name)
+print("stop")
 #2
 class Animal():
     name=""
