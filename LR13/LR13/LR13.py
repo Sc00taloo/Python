b940a80dd876664f5d1e3fede0d3b44f50e70707
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-10-03 13:07:23 +0300

    LR13.4

--- LR13/LR13/LR13.py
+++ LR13/LR13/LR13.py
@@ -1,3 +1,16 @@
+#4
+class Point:  
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+    def dist(self, point):
+        return ((self.x - point.x)**2 + (self.y - point.y)**2)**0.5
+
+pos1 = Point(3, 1)
+pos2 = Point(2, 1)
+print(pos1.dist(pos2))
+print("sTOP")
+
 #3
 class StringVar():
     def __init__(self, input_name):
@@ -25,7 +38,7 @@ test.name = "Siting"
 print(test.name)
 #test.set_name("Bruh")
 #print(test.name)
-print("stop")
+
 #2
 class Animal():
     name=""
