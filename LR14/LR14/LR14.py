a4c6802f3e90fa87821d2c8bef733a947da55730
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-10-14 23:03:59 +0300

    LR14.5

--- LR14/LR14/LR14.py
+++ LR14/LR14/LR14.py
@@ -1,3 +1,64 @@
+#5
+import tkinter
+import math
+
+def calc():
+    b = txt1.get()
+    if b.isdigit():
+        b = int(txt1.get())
+        numb.set(4/3 * math.pi * b * b * b)
+    else:
+        numb.set('Ошибка!')
+
+def click():
+    a = txt2.get()
+    if variable.get() == 'txt':
+        label3.config(text = "Сохранено")
+        f = open('TextTXT.txt','w+')
+        f.write(a)
+        f.close()
+    elif variable.get() == 'HTML':
+        label3.config(text = "Сохранено")
+        f = open('TextHTML.html','w+')
+        f.write(a)
+        f.close()
+    else:
+        label3.config(text = "Выберите формат!")
+
+OptionList = ["Выпадающий список","txt","HTML"]
+
+window = tkinter.Tk()
+frame = tkinter.Frame(window)
+frame.pack()
+
+variable = tkinter.StringVar(frame)
+variable.set(OptionList[0])
+
+opt = tkinter.OptionMenu(frame, variable, *OptionList)
+
+numb = tkinter.IntVar()
+label1 = tkinter.Label(frame, text = 'Введите радиус:')
+label2 = tkinter.Label(frame, text = 'Результат вычислений:')
+label3 = tkinter.Label(frame)
+label4 = tkinter.Label(frame)
+label5 = tkinter.Label(frame)
+txt1 = tkinter.Entry(frame)
+txt2 = tkinter.Entry(frame, textvariable = numb)
+button1 = tkinter.Button(frame, text = 'Вычислить', command = calc)
+button2 = tkinter.Button(frame, text = 'Сохранить', command = click)
+
+label1.grid(row=0,column=1)
+txt1.grid(row=0,column=2)
+label2.grid(row=1,column=1)
+txt2.grid(row=1,column=2)
+label4.grid(row=2,column=2)
+label5.grid(row=3,column=2)
+button1.place(x=115,y=50)
+button2.grid(row=4,column=1)
+opt.grid(row=4,column=2)
+
+window.mainloop()
+
 #3
 import tkinter
 
@@ -76,7 +137,7 @@ label4 = tkinter.Label(frame)
 txt = tkinter.Entry(frame)
 
 random_word()
-
+ 
 label1.pack()
 label2.pack()
 txt.pack()
