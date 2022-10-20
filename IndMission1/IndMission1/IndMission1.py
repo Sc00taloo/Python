ff808404a949bedeb2451d25bf8a8b305958ae07
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-10-21 00:45:37 +0300

    IndMission1(Complete)

--- IndMission1/IndMission1/IndMission1.py
+++ IndMission1/IndMission1/IndMission1.py
@@ -1,60 +1,63 @@
+def special(x):
+    sum = 0
+    x1 = abs(x)
+    while x1 > 0:
+        sum = sum +(x1 % 3)
+        x1 = x1 / 3
+    return sum
+
 f = open("10a.txt","r")
 f2 = open("10b.txt","r")
-#n = int(f.read(3))
-#k = int(f.read(3))
-#d = int(f.read(3))
-n = int(f2.read(6))
-k = int(f2.read(3))
-d = int(f2.read(4))
-f.read(1)
+n = int(f.read(3))
+k = int(f.read(3))
+d = int(f.read(3))
+n1 = int(f2.read(6))
+k1 = int(f2.read(3))
+d1 = int(f2.read(4))
 print(n,k,d)
-test =[]
-test1 =[]
-for numbers in f2:
-    test.append(int(numbers))
-
-count = -1
-for num in test:
-    count+=1
-    if num < 0:
-        num1 = num * -1
-        numb = 0
-        while num1 > 0:
-            numb += (num1 % 3)
-            num1//=3
-        if numb == 12:
-            test1.append(count)
-test1.append(count)
-
-if (len(test1) % k != 0):
-    j = 0
-    suma = 0
-    count = 0
-    suma_max = []
-    for num5 in test:
-        count += 1
-        suma += num5
-        if test.index(num5) == test1[j]:
-            suma += num5
-            count -= 1
-            if (count % d == 0):
-                suma_max.append(suma)
-            count = 0
-            j += 1
-            suma = 0
-else:
-    j = 0
-    suma = 0
-    count = 0
-    suma_max = []
-    for num5 in test:
-        if test.index(num5) != test1[-1]:
-            count += 1
-            suma += num5
-        else:
-            break
-
-print(suma)
+print(n1,k1,d1)
+f.read(1)
+maax = -100000000
+a = []
+a1 = []
+for i in f:
+    a.append(int(i))
+a.append(0)
+i = 0
+for i in f2:
+    a1.append(int(i))
+a1.append(0)
 
 f.close()
 f2.close()
+
+i = 0
+for i in range(n):
+    count = 1
+    countSpecial = 0
+    if special(a[i]) == 12 and a[i] < 0:
+        countSpecial = countSpecial + 1
+    sum = a[i]
+    for j in range(n):
+        count = count + 1
+        sum = sum + a[j + 1]
+        if special(a[j + 1]) == 12 and a[j + 1] < 0:
+            countSpecial = countSpecial + 1
+        if countSpecial % k == 0 and count % d == 0:
+            maax = max(maax,sum)
+print(maax)
+maax = -100000000
+for i in range(n1):
+    count = 1
+    countSpecial = 0
+    if special(a1[i]) == 12 and a1[i] < 0:
+        countSpecial = countSpecial + 1
+    sum = a1[i]
+    for j in range(n1):
+        count = count + 1
+        sum = sum + a1[j + 1]
+        if special(a1[j + 1]) == 12 and a1[j + 1] < 0:
+            countSpecial = countSpecial + 1
+        if countSpecial % k1 == 0 and count % d1 == 0:
+            maax = max(maax,sum)
+print(maax)
